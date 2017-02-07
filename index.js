e0d6bc29777820f7614fd250a22f45034bf0b859
liang869219658 <zhanliang001@gmail.com>
2017-02-07 15:10:01 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+require("babel-polyfill");
+module.exports = require("./lib").default;
+
+
---
+++ src/utils.js
@@ -0,0 +1,43 @@
+import os from 'os';
+import path from 'path';
+
+let getTmpDir = os.tmpdir || os.tmpDir;
+
+function randomString(size = 6, chars = 'abcdefghijklmnopqrstuvwxyz0123456789') {
+  let max = chars.length + 1;
+  let str = '';
+  while (size > 0) {
+    str += chars.charAt(Math.floor(Math.random() * max));
+    size--;
+  }
+  return str;
+}
+
+export function randomFilename(tmpDir = getTmpDir()) {
+  return path.resolve(tmpDir, randomString(20));
+}
+
+export function isURL (url) {
+  if (url.substr(0, 7) === 'http://') return true;
+  if (url.substr(0, 8) === 'https://') return true;
+  return false;
+}
+
+export function callbackify(fn) {
+  let argc = fn.length;
+  return (...args) => {
+    let callback = args[argc];
+    if (typeof callback !== 'function') callback = null;
+    return fn(...args)
+      .then(ret => {
+        callback && callback(null, ret);
+        return Promise.resolve(ret);
+      })
+      .catch(err => {
+        callback && callback(err);
+        return Promise.reject(err);
+      });
+  }
+}
+
+export function noop() { }
